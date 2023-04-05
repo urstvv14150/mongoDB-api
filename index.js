b057f895133640e7bad842a6c20a076912835eb9
ken <urstvv14150@gmail.com>
2023-04-05 08:44:24 +0800

    edit cors origin

--- index.js
+++ index.js
@@ -19,7 +19,14 @@ app.use(
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.use(cors())
+app.use(cors(
+  {
+    origin: ["http://localhost:3000", "https://memories-two-iota.vercel.app" ],
+    methods: "GET,POST,PUT,DELETE",
+    credentials:true
+
+  }
+))
 app.use(express.json())
 app.use(routes)
 
